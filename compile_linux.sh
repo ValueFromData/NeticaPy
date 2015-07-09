ecab6e4bff0f372e3fb2d71abd9123a8d880063a
ahmadfaizalbh <ahmadfaizalbh726@gmail.com>
2015-07-09 12:59:33 +0530

    fix sh

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,5 +1,34 @@
 #!/bin/sh
+Kernel=$(uname -s)
+case "$Kernel" in
+    Linux)  Kernel="linux"              ;;
+    Darwin) Kernel="mac"                ;;
+    FreeBSD)    Kernel="freebsd"            ;;
+* ) echo "Your Operating System -> ITS NOT SUPPORTED"   ;;
+esac
 
+echo
+echo "Operating System Kernel : $Kernel"
+echo
+# Get the machine Architecture
+Architecture=$(uname -m)
+case "$Architecture" in
+    x86)    Architecture="x86"  ;   cp -f Netica_API_504_Linux/lib/32bit/libnetica.a   Netica_API_504_Linux/lib/libnetica.a           ;;
+    ia64)   Architecture="ia64"  ;     cp -f Netica_API_504_Linux/lib/64bit/libnetica.a   Netica_API_504_Linux/lib/libnetica.a                ;;
+    i?86)   Architecture="x86"    ;    cp -f Netica_API_504_Linux/lib/32bit/libnetica.a   Netica_API_504_Linux/lib/libnetica.a                ;;
+    amd64)  Architecture="amd64" ;  cp -f Netica_API_504_Linux/lib/64bit/libnetica.a   Netica_API_504_Linux/lib/libnetica.a                       ;;
+    x86_64) Architecture="x86_64" ; cp -f Netica_API_504_Linux/lib/64bit/libnetica.a   Netica_API_504_Linux/lib/libnetica.a                       ;;
+    sparc64)    Architecture="sparc64" ;  cp -f Netica_API_504_Linux/lib/64bit/libnetica.a   Netica_API_504_Linux/lib/libnetica.a                     ;;
+* ) echo    "Your Architecture '$Architecture' -> ITS NOT SUPPORTED." ;exit  ;;
+esac
+
+echo "Operating System Architecture : $Architecture"
+
+if $(uname -m | grep '64'); then
+cp -f Netica_API_504_Linux/lib/64bit/libnetica.a Netica_API_504_Linux/lib/libnetica.a
+else
+cp -f Netica_API_504_Linux/lib/32bit/libnetica.a Netica_API_504_Linux/lib/libnetica.a
+fi
 
 echo "compiling"
 cp Netica_API_504_Linux/NeticaPy.pyx NeticaPy.pyx
